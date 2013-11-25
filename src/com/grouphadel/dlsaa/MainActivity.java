0ecce7d74ffd35b89c86ec194422b85c098f7429
Lawrence Patrick Calulo <lugkhast@gmail.com>
2013-11-26 04:30:40 +0800

    Make navigation drawer items change current screen

--- src/com/grouphadel/dlsaa/MainActivity.java
+++ src/com/grouphadel/dlsaa/MainActivity.java
@@ -22,21 +22,6 @@ import com.grouphadel.dlsaa.app.NearbyListFragment;
 import com.grouphadel.dlsaa.app.PromotionsFragment;
 
 public class MainActivity extends FragmentActivity {
-
-	/**
-	 * The {@link android.support.v4.view.PagerAdapter} that will provide
-	 * fragments for each of the sections. We use a
-	 * {@link android.support.v4.app.FragmentPagerAdapter} derivative, which
-	 * will keep every loaded fragment in memory. If this becomes too memory
-	 * intensive, it may be best to switch to a
-	 * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-	 */
-	SectionsPagerAdapter mSectionsPagerAdapter;
-
-	/**
-	 * The {@link ViewPager} that will host the section contents.
-	 */
-	private ViewPager mViewPager;
 	private String[] mSectionTitles;
 	private DrawerLayout mDrawerLayout;
 	private ListView mDrawerList;
@@ -53,6 +38,35 @@ public class MainActivity extends FragmentActivity {
 
 		mDrawerList.setAdapter(new ArrayAdapter<String>(this,
 				R.layout.list_item_nav_drawer, mSectionTitles));
+		mDrawerList
+				.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+					@Override
+					public void onItemClick(AdapterView<?> parent, View view,
+							int position, long id) {
+						selectItem(position);
+					}
+				});
+		
+		selectItem(0);
+	}
+
+	private void selectItem(int position) {
+		Fragment fragment;
+		switch (position) {
+		case 0:
+			fragment = new NearbyListFragment();
+			break;
+		case 1:
+			fragment = new PromotionsFragment();
+			break;
+		default:
+			fragment = new Fragment();
+		}
+
+		FragmentManager fm = getSupportFragmentManager();
+		fm.beginTransaction().replace(R.id.content_frame, fragment).commit();
+		
+		mDrawerLayout.closeDrawer(mDrawerList);
 	}
 
 	@Override
@@ -60,91 +74,6 @@ public class MainActivity extends FragmentActivity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
-	}
-
-	/**
-	 * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-	 * one of the sections/tabs/pages.
-	 */
-	public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-		public SectionsPagerAdapter(FragmentManager fm) {
-			super(fm);
-		}
-
-		@Override
-		public Fragment getItem(int position) {
-			// getItem is called to instantiate the fragment for the given page.
-			Fragment fragment;
-
-			switch (position) {
-			case 0:
-				fragment = new NearbyListFragment();
-				break;
-			case 1:
-				fragment = new PromotionsFragment();
-				break;
-			default:
-				// Return a DummySectionFragment (defined as a static inner
-				// class
-				// below) with the page number as its lone argument.
-				fragment = new DummySectionFragment();
-				Bundle args = new Bundle();
-				args.putInt(DummySectionFragment.ARG_SECTION_NUMBER,
-						position + 1);
-				fragment.setArguments(args);
-				break;
-			}
-
-			return fragment;
-		}
-
-		@Override
-		public int getCount() {
-			// Show 3 total pages.
-			return 3;
-		}
-
-		@Override
-		public CharSequence getPageTitle(int position) {
-			Locale l = Locale.getDefault();
-			switch (position) {
-			case 0:
-				return getString(R.string.title_nearby).toUpperCase(l);
-			case 1:
-				return getString(R.string.title_promos).toUpperCase(l);
-			case 2:
-				return getString(R.string.title_events).toUpperCase(l);
-			}
-			return null;
-		}
-	}
-
-	/**
-	 * A dummy fragment representing a section of the app, but that simply
-	 * displays dummy text.
-	 */
-	public static class DummySectionFragment extends Fragment {
-		/**
-		 * The fragment argument representing the section number for this
-		 * fragment.
-		 */
-		public static final String ARG_SECTION_NUMBER = "section_number";
-
-		public DummySectionFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main_dummy,
-					container, false);
-			TextView dummyTextView = (TextView) rootView
-					.findViewById(R.id.section_label);
-			dummyTextView.setText(Integer.toString(getArguments().getInt(
-					ARG_SECTION_NUMBER)));
-			return rootView;
-		}
 	}
 
 }
