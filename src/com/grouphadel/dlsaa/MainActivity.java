72485b6ddfd64bf3c976c7347e43e38ba14bf84c
Lawrence Patrick Calulo <lugkhast@gmail.com>
2013-11-26 04:47:08 +0800

    Add hamburger icon and drawer shadow

--- src/com/grouphadel/dlsaa/MainActivity.java
+++ src/com/grouphadel/dlsaa/MainActivity.java
@@ -2,15 +2,19 @@ package com.grouphadel.dlsaa;
 
 import java.util.Locale;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -25,6 +29,7 @@ public class MainActivity extends FragmentActivity {
 	private String[] mSectionTitles;
 	private DrawerLayout mDrawerLayout;
 	private ListView mDrawerList;
+	private ActionBarDrawerToggle mDrawerToggle;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +39,31 @@ public class MainActivity extends FragmentActivity {
 		mSectionTitles = getResources().getStringArray(
 				R.array.nav_drawer_options);
 		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+		mDrawerToggle = new ActionBarDrawerToggle(this, /* host Activity */
+		mDrawerLayout, /* DrawerLayout object */
+		R.drawable.ic_drawer, /* nav drawer icon to replace 'Up' caret */
+		R.string.drawer_open, /* "open drawer" description */
+		R.string.drawer_close /* "close drawer" description */
+		) {
+
+			/** Called when a drawer has settled in a completely closed state. */
+			public void onDrawerClosed(View view) {
+
+			}
+
+			/** Called when a drawer has settled in a completely open state. */
+			public void onDrawerOpened(View drawerView) {
+
+			}
+		};
+
+		mDrawerLayout.setDrawerListener(mDrawerToggle);
+		
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+		getActionBar().setHomeButtonEnabled(true);
+		
+		mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+
 		mDrawerList = (ListView) findViewById(R.id.left_drawer);
 
 		mDrawerList.setAdapter(new ArrayAdapter<String>(this,
@@ -46,8 +76,22 @@ public class MainActivity extends FragmentActivity {
 						selectItem(position);
 					}
 				});
-		
+
 		selectItem(0);
+	}
+
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+
+		mDrawerToggle.syncState();
+	}
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+
+		mDrawerToggle.onConfigurationChanged(newConfig);
 	}
 
 	private void selectItem(int position) {
@@ -65,7 +109,7 @@ public class MainActivity extends FragmentActivity {
 
 		FragmentManager fm = getSupportFragmentManager();
 		fm.beginTransaction().replace(R.id.content_frame, fragment).commit();
-		
+
 		mDrawerLayout.closeDrawer(mDrawerList);
 	}
 
@@ -74,6 +118,19 @@ public class MainActivity extends FragmentActivity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Pass the event to ActionBarDrawerToggle, if it returns
+		// true, then it has handled the app icon touch event
+		if (mDrawerToggle.onOptionsItemSelected(item)) {
+			return true;
+		}
+		
+		// insert other options handlers here
+		
+		return super.onOptionsItemSelected(item);
 	}
 
 }
