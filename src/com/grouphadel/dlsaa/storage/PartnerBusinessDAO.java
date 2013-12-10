2315515ecb6c9fcafa89f6d933a476c2ad4ede7d
Dennis Abad <dennis.abad6@gmail.com>
2013-12-10 16:16:08 +0800

    getNearBusiness using long and lat of both coordinates
    Where you are and where the business is

--- src/com/grouphadel/dlsaa/storage/PartnerBusinessDAO.java
+++ src/com/grouphadel/dlsaa/storage/PartnerBusinessDAO.java
@@ -111,4 +111,16 @@ public class PartnerBusinessDAO extends BaseDAO {
 		business.setDiscountInfo("10% cash, 5% credit card. Not valid on promo");
 		this.add(business);
 	}
+	
+	public double getNearBusinesses(double lat1, double lon1, double lat2, double lon2){
+		double R = 6378.137;
+		double dLat = (lat2 - lat1) * Math.PI / 180;
+		double dLon = (lon2 - lon1) * Math.PI / 180;
+		double a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+		
+		double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+		double d = R * c;
+		
+		return d * 1000;//in meters
+	}
 }
