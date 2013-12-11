b6aad17cb8aed77196bc03cc1f3aebda129bbf02
Lawrence Patrick Calulo <lugkhast@gmail.com>
2013-12-11 19:17:54 +0800

    Organize imports in MainActivity

--- src/com/grouphadel/dlsaa/MainActivity.java
+++ src/com/grouphadel/dlsaa/MainActivity.java
@@ -1,29 +1,22 @@
 package com.grouphadel.dlsaa;
 
-import java.util.Locale;
-
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.GravityCompat;
-import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
-import com.grouphadel.dlsaa.app.NearbyListFragment;
 import com.grouphadel.dlsaa.app.ChapterLocatorFragment;
+import com.grouphadel.dlsaa.app.NearbyListFragment;
 import com.grouphadel.dlsaa.app.PetronCardRegistrationFragment;
 
 public class MainActivity extends FragmentActivity {
