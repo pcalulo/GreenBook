c26a0a070cca67a5df24bd0cc2f02c59fede9c06
Dennis Abad <dennis.abad6@gmail.com>
2013-12-12 05:38:43 +0800

    Partner Branch Object

--- src/com/grouphadel/dlsaa/models/PartnerBranch.java
+++ src/com/grouphadel/dlsaa/models/PartnerBranch.java
@@ -1,5 +1,58 @@
 package com.grouphadel.dlsaa.models;
 
-public class PartnerBranch {
+public class PartnerBranch extends DatabaseObject{
+	private int businessID;
+	private String address;
+	private double longitude;
+	private double latitude;
+	
+	public PartnerBranch() {
+		this.businessID = 0;
+		this.address = "";
+		this.longitude = 0;
+		this.latitude = 0;
+	}
+	
+	public PartnerBranch(int businessID, String address, double longitude,
+			double latitude) {
+		super();
+		this.businessID = businessID;
+		this.address = address;
+		this.longitude = longitude;
+		this.latitude = latitude;
+	}
 
+	public int getBusinessID() {
+		return businessID;
+	}
+	
+	public void setBusinessID(int businessID) {
+		this.businessID = businessID;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public double getLongitude() {
+		return longitude;
+	}
+	
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+	
+	public double getLatitude() {
+		return latitude;
+	}
+	
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+	
+	
 }
